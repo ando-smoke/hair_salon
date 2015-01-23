0d985bfb1867d4015578afee5d92e5cf6d902bc6
Andy Uppendahl <a.uppendahl@gmail.com>
2015-01-23 10:36:22 -0800

    Add 'all' class method with related spec.

---
+++ lib/stylist.rb
@@ -0,0 +1,22 @@
+class Stylist
+
+  define_method(:initialize) do |attributes|
+    @id = attributes[:id]
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+  end
+
+  define_singleton_method(:all) do
+    returned_stylists = DB.exec("SELECT * FROM stylists;")
+    stylists = []
+    returned_stylists.each() do |stylist|
+      id = stylist["id"].to_i()
+      first_name = stylist["first_name"]
+      last_name = stylist["last_name"]
+      stylists.push(Stylist.new({ :id => id,
+        :first_name => first_name, :last_name => last_name}))
+    end
+    stylists
+  end
+
+end
