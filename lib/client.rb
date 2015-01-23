323d5cecada5a23210aab9e39c5d5107eb838c44
Andy Uppendahl <a.uppendahl@gmail.com>
2015-01-23 09:47:19 -0800

    Add 'all' class method with related spec.

---
+++ lib/client.rb
@@ -0,0 +1,25 @@
+class Client
+
+  define_method(:initialize) do |attributes|
+    @id = attributes[:id]
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @stylist_id = attributes[:stylist_id]
+  end
+
+  define_singleton_method(:all) do
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    clients = []
+    returned_clients.each() do |client|
+      id = client["id"]
+      first_name = client["first_name"]
+      last_name = client["last_name"]
+      stylist_id = client["stylist_id"]
+      clients.push(Client.new({ :id => id,
+        :first_name => first_name, :last_name => last_name,
+        :stylist_id => stylist_id }))
+    end
+    clients
+  end
+
+end
